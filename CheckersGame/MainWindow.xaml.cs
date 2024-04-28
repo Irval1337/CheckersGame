c30ff8ab1b55b89cd3b80ea61c31c13d1ee3c3c3
Irval <56792892+Irval1337@users.noreply.github.com>
2024-04-28 23:21:02 +0300

    Some design improvements

--- CheckersGame/MainWindow.xaml.cs
+++ CheckersGame/MainWindow.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace CheckersGame
 {
